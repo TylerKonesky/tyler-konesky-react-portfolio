97d48414ee8b17c82e0ccd5311c62e53c88b1990
Tyler <tylerkonesky@hotmail.com>
2019-10-17 15:29:55 -0600

    fancy HTML stuff

--- src/components/app.js
+++ src/components/app.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import axios from "axios"
 
-import { library } from "@fortawesome/fontawesome-svg-core";
+// import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt, faEdit, faSpinner } from "@fortawesome/free-solid-svg-icons";
+// import { faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 
 import PortfolioContainer from './portfolio-container';
 import NavigationContainer from './navigation-container'
@@ -18,12 +18,15 @@ import NoMatch from "./Pages/no-match"
 import PortfolioManager from "./pages/portfolio-manager";
 import BlogDetail from "../components/Pages/blog-detail"
 
-library.add(faTrash, faSignOutAlt, faEdit, faSpinner)
+// library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle)
+import Icons from "../helpers/helpers"
 
 
 export default class App extends Component {
   constructor(props){
     super(props);
+
+    Icons();
 
     this.state ={
       loggedInStatus: "NOT_LOGGED_IN"
@@ -118,7 +121,13 @@ export default class App extends Component {
                 <Route path="/contact" component={Contact}></Route>
                 {this.state.loggedInStatus === "LOGGED_IN" ? (this.authorizedPages()) : null}
                 <Route path="/portfolio/:slug" component={PortfolioDetail} />
-                <Route path="/blog" component={Blog} />
+                <Route 
+                    path="/blog" 
+                    render={props => 
+                    <Blog {...props} loggedInStatus={this.state.loggedInStatus} />
+                    }
+                    
+                />
                 <Route path="/b/:slug" component={BlogDetail} />
                 <Route path="/portfolio-manager" component={PortfolioManager} />
                 <Route component={NoMatch}></Route>
